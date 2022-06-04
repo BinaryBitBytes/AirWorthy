f9cbcc5ccbcee7135d0fa6d86dd2f301e330b856
Emmanuela Jeanbaptiste <emmanuelajeanbaptiste1@gmail.com>
2022-06-04 11:31:24 -0400

    changes to file struct

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const doctorRoutes = require('./doctorRoutes');
-
-router.use('/users', userRoutes);
-router.use('/doctor', doctorRoutes);
-
-module.exports = router;
