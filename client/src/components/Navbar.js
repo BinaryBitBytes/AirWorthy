6b5279e5772549e188152d51bfc87d4e02a54ba2
Emmanuela Jeanbaptiste <emmanuelajeanbaptiste1@gmail.com>
2022-06-04 13:37:59 -0400

    changes to components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,77 +1,131 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from './SignupForm';
-import LoginForm from './LoginForm';
+import React from "react";
+import $ from "jquery";
 
-import Auth from '../utils/auth';
+class Navbar extends React.Component {
+  componentDidMount() {
+    const nav = $("nav");
+    let navHeight = nav.outerHeight();
 
-const AppNavbar = () => {
-  // set modal display state
-  const [showModal, setShowModal] = useState(false);
+    $(".navbar-toggler").on("click", function () {
+      if (!$("#mainNav").hasClass("navbar-reduce")) {
+        $("#mainNav").addClass("navbar-reduce");
+      }
+    });
 
-  return (
-    <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            Google Books Search
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar'>
-            <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='/'>
-                Search For Books
-              </Nav.Link>
-              {/* if user is logged in show saved books and logout */}
-              {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to='/saved'>
-                    See Your Books
-                  </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      {/* set modal data up */}
-      <Modal
-        size='lg'
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey='login'>
-          <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
-    </>
-  );
-};
+    $("body").scrollspy({
+      target: "#mainNav",
+      offset: navHeight,
+    });
 
-export default AppNavbar;
+    $(".js-scroll").on("click", function () {
+      $(".navbar-collapse").collapse("hide");
+    });
+
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 50) {
+        document
+          .querySelector(".navbar-expand-md")
+          .classList.add("navbar-reduce");
+        document
+          .querySelector(".navbar-expand-md")
+          .classList.remove("navbar-trans");
+      } else {
+        document
+          .querySelector(".navbar-expand-md")
+          .classList.add("navbar-trans");
+        document
+          .querySelector(".navbar-expand-md")
+          .classList.remove("navbar-reduce");
+      }
+    });
+
+    $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+      if (
+        window.location.pathname.replace(/^\//, "") ===
+          this.pathname.replace(/^\//, "") &&
+        window.location.hostname === this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top - navHeight + 5,
+            },
+            1000,
+            "easeInExpo"
+          );
+          return false;
+        }
+      }
+    });
+
+    $(".js-scroll").on("click", function () {
+      $(".navbar-collapse").collapse("hide");
+    });
+  }
+
+  render() {
+    return (
+      <nav
+        className="navbar navbar-b navbar-trans navbar-expand-md fixed-top"
+        id="mainNav"
+      >
+        <div className="container">
+          <div class="d-flex navbar navbar-expand-lg">Air Worthy</div>
+          {/* <a className="navbar-brand js-scroll" href="#page-top">
+            <img
+              src={this.state.logo}
+              alt="logo"
+              style={{ maxWidth: "100px" }}
+            />
+          </a> */}
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarDefault"
+            aria-controls="navbarDefault"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <div
+            className="navbar-collapse collapse justify-content-end"
+            id="navbarDefault"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link js-scroll active" href="#home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll" href="#work">
+                  Work
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link js-scroll" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
