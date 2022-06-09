817b7bf3d88763e2dc45d859e57c3e0b07b5fdcb
Emmanuela Jeanbaptiste <emmanuelajeanbaptiste1@gmail.com>
2022-06-08 20:51:12 -0400

    changes to sever files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,104 +15,83 @@ const technicianResolvers = {
       return Technician.findOne({ _id: technicianID });
     },
   },
-  
+
   Mutation: {
-    addTechnician: async(parent, {technicianName, isAdmin, onProject, username, email, password}) => {
-      return Technician.create({ technicianName, isAdmin, onProject, username, email, password });
+    addTechnician: async (
+      parent,
+      { technicianName, isAdmin, onProject, username, email, password }
+    ) => {
+      return Technician.create({
+        technicianName,
+        isAdmin,
+        onProject,
+        username,
+        email,
+        password,
+      });
     },
-    addProject: async(parent, {technicianID, onProject }) => {
+    addProject: async (parent, { technicianID, onProject }) => {
       return Technician.findOneAndUpdate(
-        {_id: technicianID},
+        { _id: technicianID },
         {
-          $addToSet: { project: {onProject} },
+          $addToSet: { project: { onProject } },
         },
         {
           new: true,
           runValidators: true,
-        },
+        }
       );
     },
-    removeTechnician: async (parent, { technicianID }) =>{
-      return Technician.fineOneAndDelete({ _id: technicianID});
+    removeTechnician: async (parent, { technicianID }) => {
+      return Technician.fineOneAndDelete({ _id: technicianID });
     },
-    removeTechnician: async(parent, { technicianId, onProject }) => {
-      return Technician.findOneAndUpdate(
-        { _id: technicianId},
-        {}
-      )
+    removeTechnician: async (parent, { technicianId, onProject }) => {
+      return Technician.findOneAndUpdate({ _id: technicianId }, {});
     },
-  }
+  },
 };
 
 module.exports = technicianResolvers;
 
-
-
-
-
-
-
-
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
-const { AuthenticationError } = require('apollo-server-express');
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
-    me: async (parent, {userId}) => {
-      return await User.findOne(
-        { $or: [{ _id: userId}, { username: username }] }
-      ).populate('savedBooks');
+    me: async (parent, { userId }) => {
+      return await User.findOne({
+        $or: [{ _id: userId }, { username: username }],
+      }).populate("savedBooks");
     },
   },
 
   Mutation: {
-    loginUser: async (parent,  { email, password }) => {
+    loginUser: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError('Cannot find this user');
+        throw new AuthenticationError("Cannot find this user");
       }
-  
+
       const correctPw = await user.isCorrectPassword(password);
-  
+
       if (!correctPw) {
-        throw new AuthenticationError('Wrong password!');
-      }
-      const token = signToken(user);
-      return({ token, user });
-    },
-  
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-  
-      if (!user) {
-        return res.status(400).json({ message: 'Something is wrong!' });
+        throw new AuthenticationError("Wrong password!");
       }
       const token = signToken(user);
       return { token, user };
     },
 
-    saveBook: async (parent, {userId, authors, description, bookId, image, link, title}) => {
-        try {
-        return await User.findOneAndUpdate(
-          { _id: userId },
-          { $addToSet: { savedBooks: {authors, description, bookId, image, link, title}, } },
-          { new: true, runValidators: true }
-        );
-      } catch (err) {
-        console.log(err);
-        return res.status(400).json(err);
-      }
-    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
 
-    removeBook: async (parents, {userId, bookId}) => {
-      return await User.findOneAndUpdate(
-        { _id: userId },
-        { $pull: { savedBooks: { bookId: args.bookId } } },
-        { new: true, runValidators: true }
-        );
-    }
+      if (!user) {
+        return res.status(400).json({ message: "Something is wrong!" });
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
   },
-}
+};
 
 module.exports = resolvers;
