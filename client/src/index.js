1a837eb2b838202715ac05cd83404fe6537a546a
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-09 22:52:28 -0400

    seen that the client/src/index.js import dependancy had an error in the import CSS because there was a forward slask \ instead of a backslash that occured from VS codes feature that copied relative path.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import CSS from "client\src\index.css";
+import CSS from "client/src/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
