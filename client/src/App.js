e13e7efd00b2d7e25f8be1feeee12a37fd193b8a
Emmanuela Jeanbaptiste <emmanuelajeanbaptiste1@gmail.com>
2022-06-10 23:00:13 -0400

    update react page and app.js to route to right pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper";
+
 import Intro from "./components/intro";
+import Manager from "./components/Manager";
+import Technician from "./components/Technician";
 
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-      <Intro></Intro>
-
-      <Footer></Footer>
-    </>
+    <div className="App">
+      <BrowserRouter basename="/">
+        <Navbar></Navbar>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Intro />} />
+            <Route path="/manager" element={<Manager />} />
+            <Route path="/technician" element={<Technician />} />
+          </Routes>
+        </div>
+        <Footer></Footer>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ client/src/pages/Manager.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Manager from "./components/Manager.js";
+
+function Manager() {
+  return <Manager />;
+}
+
+{
+  /* <React.Fragment>
+  <Manager />
+</React.Fragment>; */
+}
+
+export default Manager;
