8000f3d6597b7006a70999728f9cbbbf1bea3f06
Emmanuela Jeanbaptiste <emmanuelajeanbaptiste1@gmail.com>
2022-06-10 20:28:56 -0400

    home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Intro from "./components/intro";
-import About from "./components/about";
-import Contact from "./components/contact.js";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Intro></Intro>
-      <About></About>
-      <Contact></Contact>
+
       <Footer></Footer>
     </>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,69 +1,8 @@
 import React from "react";
-import $ from "jquery";
+import "react-bootstrap";
 import "./stars.scss";
 
 class Navbar extends React.Component {
-  componentDidMount() {
-    $(".navbar-toggler").on("click", function () {
-      if (!$("#mainNav").hasClass("navbar-reduce")) {
-        $("#mainNav").addClass("navbar-reduce");
-      }
-    });
-
-    // $("body").scrollspy({
-    //   target: "#mainNav",
-    // });
-
-    //   // $(".js-scroll").on("click", function() {
-    //   //   $(".navbar-collapse").collapse("hide");
-    //   // });
-
-    //   window.addEventListener("scroll", () => {
-    //     if (window.pageYOffset > 50) {
-    //       document
-    //         .querySelector(".navbar-expand-md")
-    //         .classList.add("navbar-reduce");
-    //       document
-    //         .querySelector(".navbar-expand-md")
-    //         .classList.remove("navbar-trans");
-    //     } else {
-    //       document
-    //         .querySelector(".navbar-expand-md")
-    //         .classList.add("navbar-trans");
-    //       document
-    //         .querySelector(".navbar-expand-md")
-    //         .classList.remove("navbar-reduce");
-    //     }
-    //   });
-
-    // $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function() {
-    //   if (
-    //     window.location.pathname.replace(/^\//, "") ===
-    //       this.pathname.replace(/^\//, "") &&
-    //     window.location.hostname === this.hostname
-    //   ) {
-    //     var target = $(this.hash);
-    //     target = target.length
-    //       ? target
-    //       : $("[name=" + this.hash.slice(1) + "]");
-    //     if (target.length) {
-    //       $("html, body").animate(
-    //         {
-    //           scrollTop: target.offset().top - navHeight + 5
-    //         },
-    //         1000,
-    //         "easeInExpo"
-    //       );
-    //       return false;
-    //     }
-    //   }
-    // });
-
-    // $(".js-scroll").on("click", function() {
-    //   $(".navbar-collapse").collapse("hide");
-    // });
-  }
-
   render() {
     return (
       <nav
@@ -96,21 +35,21 @@ class Navbar extends React.Component {
                   Home
                 </a>
               </li>
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <a className="nav-link " href="#about">
                   About
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link " href="#contact">
-                  Employee Portal
+                 
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link " href="#contact">
-                  Contact
+                 
                 </a>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
