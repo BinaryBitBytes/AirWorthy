07fb75363b3f5a28e2fb29868c10a20650ffd227
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-11 22:54:43 -0400

    re added the file due to pull origin main deleted the entry for controllers in the routes

---
+++ client/src/Utils/api.js
@@ -0,0 +1,149 @@
+//Get, Put, Post Technician Data
+export const getTechnician = () => {
+    return fetch('/api/technician', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+};
+
+export const createTechnician = (technicianData) => {
+    return fetch(`/api/technician/${technicianData}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(technicianData),
+    })
+};
+
+export const saveTechnician = (technicianData) => {
+    return fetch('/api/technician', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(technicianData),
+    });
+};
+
+//Get, Put, Post Manager Data
+export const getManager = () => {
+    return fetch('/api/manager', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+};
+
+export const createManager = (managerData) => {
+    return fetch(`/api/manager/${managerData}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(managerData),
+    })
+};
+
+export const saveManager = (managerData) => {
+    return fetch('/api/manager', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(managerData),
+    });
+};
+
+//Get, Put, Post Airliner Data
+export const getAirliner = () => {
+    return fetch('/api/airliner', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+};
+
+export const createAirliner = (airlinerData) => {
+    return fetch(`/api/airliner/${airlinerData}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(airlinerData),
+    })
+};
+
+export const saveAirliner = (airlinerData) => {
+    return fetch('/api/airliner', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(airlinerData),
+    });
+};
+
+//Get, Put, Post Inspector Data
+export const getInspector = () => {
+    return fetch('/api/inspector', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+};
+
+export const createInspector = (inspectorData) => {
+    return fetch(`/api/inspector/${inspectorData}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inspectorData),
+    })
+};
+
+export const saveInspector = (inspectorData) => {
+    return fetch('/api/inspector', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inspectorData),
+    });
+};
+
+//Get, Put, Post Project Data
+export const getProject = () => {
+    return fetch('/api/project', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+};
+
+export const createProject = (projectData) => {
+    return fetch(`/api/project/${projectData}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(projectData),
+    })
+};
+
+export const saveProject = (projectData) => {
+    return fetch('/api/project', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'project/json'
+        },
+        body: JSON.stringify(projectData),
+    });
+};
