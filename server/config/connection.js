ca00b19698598deefc0748ba659c744677919454
BinaryBitBytes <117mwc117@gmail.com>
2023-05-12 19:50:40 -0400

    wrapped mongoose.connection with a main function to deploy it asychronously. created an export var, used async / await

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/AirWorthy_DB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+main().catch(err => console.log(err)) // added a catch error statement to log the error.
+
+async function main(){
+  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/AirWorthy_DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} //! Updated 5.12.23 - encapsulated the mongoose.connection function asyncronously 
+// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 
 module.exports = mongoose.connection;
+module.exports = main(); // updated 5.12.23, this function is to deplot mongoose asynchronously.
