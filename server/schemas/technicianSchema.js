9589c51e97f3de5971e14559734fae8e81f5441f
BinaryBitBytes <117mwc117@gmail.com>
2023-05-17 01:23:05 -0400

    for scaling started blending the type defs and resolvers within each respected schema. already blended Technician & Manager. Still need to blend the rest of the schemas. take note that in managerSchema.js is throwing flag suggesting correction to manager_id. be aware of this for seeding and debugging

--- server/schemas/technicianSchema.js
+++ server/schemas/technicianSchema.js
@@ -8,3 +8,53 @@ type Technician {
     email: String
     password: String
 ` ;
+
+export const resolvers = {
+  Technician: {
+      Query: 
+      {
+        technicians: async () => {
+          return await Technician.find().sort({ createdAt: -1 })//.cursor(); //added .cursor to see if this resolves //! added await
+        },
+  
+        technician: async (parent, { technicianID }) => {
+          return await Technician.findOne({ _id: technicianID })//.cursor(); //added .cursor to see if this resolves //! added await
+        },
+      },
+  
+      Mutation: 
+      {
+        addTechnician: async (
+          parent,
+          { technicianName, isAdmin, onProject, username, email, password }
+        ) => {
+          return Technician.create({
+            technicianName,
+            isAdmin,
+            onProject,
+            username,
+            email,
+            password,
+          });
+        },
+        addProject: async (parent, { technicianID, onProject }) => {
+          return Technician.findOneAndUpdate(
+            { _id: technicianID },
+            {
+              $addToSet: { project: { onProject } },
+            },
+            {
+              new: true,
+              runValidators: true,
+            }
+          );
+        },
+        removeTechnician: async (parent, { technicianID }) => {
+          return Technician.fineOneAndDelete({ _id: technicianID });
+        },
+        removeTechnician: async (parent, { technicianID, onProject }) => {
+          return Technician.destroy({ _id: technicianID }, {});
+        },
+      },
+  }
+};
