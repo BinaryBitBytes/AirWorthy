eb512132b7614f84943c39ad2ae3d00c9101e573
BinaryBitBytes <117mwc117@gmail.com>
2023-05-18 00:09:14 -0400

    added inspector resolvers to the inspector schema js as a constant to resolvers for exporting and scaling

--- server/schemas/inspectorSchema.js
+++ server/schemas/inspectorSchema.js
@@ -9,3 +9,58 @@ type Inspector {
     password: String
   }
 `;
+
+export const resolvers = {
+  Inspector: {
+    Query: 
+    {
+      inspectors: async () => 
+      {
+        return await Technician.find().sort({ createdAt: -1 }); //! added await
+      },
+
+      inspector: async (parent, { inspectorID }) => 
+      {
+        return await Technician.findOne({ _id: inspectorID }); //! added await
+      },
+    },
+
+    Mutation: 
+    {
+      addInspector: async (
+        parent,
+        { inspectorName, isAdmin, onProject, username, email, password }
+      ) => {
+        return Inspector.create({
+          inspectorName,
+          isAdmin,
+          onProject,
+          username,
+          email,
+          password,
+        });
+      },
+      addProject: async (parent, { inspectorID, onProject }) => 
+      {
+        return Inspector.findOneAndUpdate(
+          { _id: inspectorID },
+          {
+            $addToSet: { project: { onProject } },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      },
+      removeInspector: async (parent, { inspectorID }) => 
+      {
+        return Inspector.fineOneAndDelete({ _id: inspectorID });
+      },
+      removeInspector: async (parent, { inspectorID, onProject }) => 
+      {
+        return Inspector.destroy({ _id: inspectorID }, {});
+      },
+    },
+  }
+};
