7a70e9c6806d9dc782959078765237f1c6893ff4
BinaryBitBytes <117mwc117@gmail.com>
2023-05-18 00:02:32 -0400

    added airliner resolvers to the airliner schema js as a constant to resolvers for exporting and scaling

--- server/schemas/airlinerSchema.js
+++ server/schemas/airlinerSchema.js
@@ -9,3 +9,57 @@ export const typeDef = `
         password: String
   }
 ` ;
+
+export const resolvers = {
+  Airliner: {
+    Query: 
+    {
+      airliners: async () => {
+        return await Airliner.find().sort({ createdAt: -1 }); //! added await
+      },
+
+      airliner: async (parent, { airlinerID }) => {
+        return await Airliner.findOne({ _id: airlinerID }); //! added await
+      },
+    },
+
+    Mutation: 
+    {
+      addAirliner: async (parent,
+        { airlinerName, isAdmin, modelAircraft, username, email, password }
+      ) => {
+        return Airliner.create(
+          {
+          airlinerName,
+          isAdmin,
+          modelAircraft,
+          email,
+          username,
+          password,
+          }
+        );
+      },
+      addAirliner: async (parent, { airlinerID, airlinerName }) => 
+      {
+        return Airliner.findOneAndUpdate(
+          { _id: airlinerID },
+          {
+            $addToSet: { airlinerName: { airlinerName } },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      },
+      removeAirliner: async (parent, { airlinerID }) => 
+        {
+          return Airliner.fineOneAndDelete({ _id: airlinerID });
+        },
+      removeAirliner: async (parent, { airlinerID, airlinerName }) => 
+        {
+          return Airliner.destroy({ _id: airlinerID }, { $airlinerName });
+        },
+    },
+  }
+};
