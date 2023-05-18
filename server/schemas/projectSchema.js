c1cd0cdfd7ceb5348279c5f80d1ba241f407a7b5
BinaryBitBytes <117mwc117@gmail.com>
2023-05-18 00:06:13 -0400

    added project resolvers to the project schema js as a constant to resolvers for exporting and scaling

--- server/schemas/projectSchema.js
+++ server/schemas/projectSchema.js
@@ -5,4 +5,53 @@ type Project {
     inspectorName: String
     workDescription: String
   }
-` ;
+` 
+export const resolvers =  {
+  Project: {
+    Query: 
+    {
+      projects: async () => {
+        return await Project.find().sort({ createdAt: -1 }); //! added await
+      },
+
+      project: async (parent, { projectID }) => {
+        return await Project.findOne({ _id: projectID }); //! added await
+      },
+    },
+
+    Mutation: 
+    {
+      addProject: async (
+        parent,
+        { projectName, isAdmin, onProject, username, email, password }
+      ) => {
+        return Project.create({
+          projectName,
+          isAdmin,
+          onProject,
+          username,
+          email,
+          password,
+        });
+      },
+      addProject: async (parent, { projectID, onProject }) => {
+        return Project.findOneAndUpdate(
+          { _id:projectID },
+          {
+            $addToSet: { project: { onProject } },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      },
+      removeProject: async (parent, { projectID }) => {
+        return Project.fineOneAndDelete({ _id: projectID });
+      },
+      removeTechnician: async (parent, { projectId, onProject }) => {
+        return Project.destroy({ _id: projectId }, {});
+      },
+    },
+  }
+};
