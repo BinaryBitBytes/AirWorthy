5961d05b71ed30e1283296bf90fb6917226a0201
BinaryBitBytes <117mwc117@gmail.com>
2023-05-30 14:55:18 -0400

    removed comments that tried to export the typedefs in typeDef folder index.js

--- server/src/typeDef-Resolvers/index.js
+++ server/src/typeDef-Resolvers/index.js
@@ -9,70 +9,8 @@
 // import { typeDef as Technician, resolvers as technicianResolvers } from './technicianSchema.js';
 import { resolvers } from './resolvers.js';
 import { typeDef } from './typeDef.js';
-export const  resolver = resolvers;
+export const resolver = resolvers;
 export const typeDefs = typeDef;
 
 console.log(typeDefs);
 console.log(resolvers);
-// module.exports = {
-//     typeDef, 
-//     resolvers}
-//! export default () =>{
-//     return { typeDef, resolvers}
-//! };
-
-
-// export const typeDef= {
-    //     Airliner,
-//     Auth,
-//     Inspector,
-//     Manager,
-//     ProjectData,
-//     Project,
-//     Technician
-// };
-// console.log('index .js typeDef')
-// console.log(typeDef);
-// export default function typeDef() {
-//     const all = (
-//     Airliner,
-//     Auth,
-//     Inspector,
-//     Manager,
-//     ProjectData,
-//     Project,
-//     Technician)
-//     return all;
-// }
-
-
-// export const resolvers = [
-//     airlinerResolvers,
-//     authResolvers,
-//     inspectorResolvers,
-//     managerResolvers,
-//     projectDataResolvers,
-//     projectResolvers,
-//     technicianResolvers
-// ]
-
-// export function resolvers() {
-//     const all = (airlinerResolvers,
-//     authResolvers,
-//     inspectorResolvers,
-//     managerResolvers,
-//     projectDataResolvers,
-//     projectResolvers,
-//     technicianResolvers)
-//     return all;
-// }
-
-
-// module.exports = {
-//         typeDef,
-//         resolvers
-//     }
-// export default typeDef;
-// export default resolvers;
-// module.exports.typeDef = (Airliner, Auth, Inspector, Manager, ProjectData, Project, Technician );
-// module.exports.resolvers = { airlinerResolvers, authResolvers, inspectorResolvers, managerResolvers, projectDataResolvers, projectResolvers, technicianResolvers };
