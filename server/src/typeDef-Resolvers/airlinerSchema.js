b95a09bfd07b3d20292154e26a4ad91284873725
BinaryBitBytes <117mwc117@gmail.com>
2023-05-31 15:55:15 -0400

    was able to start resolving the query in my typeDefs & resolvers thanks to referring to the conventions used as a reference example by @Jonathan-Warkentine re-engage project in his schemas folder for following best practices as referenced at these links {https://github.com/jonathan-warkentine/re-engage/blob/main/server/schemas/typeDefs/typeDefs.js} & {https://github.com/jonathan-warkentine/re-engage/blob/main/server/schemas/resolvers/passageResolvers.js}

--- server/src/typeDef-Resolvers/airlinerSchema.js
+++ server/src/typeDef-Resolvers/airlinerSchema.js
@@ -1,18 +1,19 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import pkg from 'apollo-server-express';
 const { gql } = pkg;
+// import { airlinerSchema } from '../models/Airliner.js';
+import { typeDefs as Auth } from './authSchema.js';
 
 export const resolvers = {
   Airliner: {
-    Query:
-    {
-      airliners: async () => {
-        return await Airliner.find().sort({ createdAt: -1 }); //! added await
+    Query: {
+      airliner: async (parent, { _id }) => {
+        return Airliner.findOne({ _id: airlinerID }).populate("airliner"); //! added await
+      },
+      airliners: async (parent, { airlinerName }) => {
+        return Airliner.find().sort({ createdAt: -1 }).populate("airliner"); //! added await
       },
 
-      airliner: async (parent, { airlinerID }) => {
-        return await Airliner.findOne({ _id: airlinerID }); //! added await
-      },
     },
 
     Mutation:
@@ -20,19 +21,23 @@ export const resolvers = {
       addAirliner: async (parent,
         { airlinerName, isAdmin, modelAircraft, username, email, password }
       ) => {
-        return Airliner.create(
-          {
-            airlinerName,
-            isAdmin,
-            modelAircraft,
-            email,
-            username,
-            password,
-          }
-        );
+        const newAirliner = new Airliner({ airlinerName, isAdmin, modelAircraft, username, email, password });
+        await newAirliner.build(airlinerName, username, email, password);
+        await newAirliner.save();
+        await newAirliner.populate("airliner");
+        // return Airliner.create(
+        //   {
+        //     airlinerName,
+        //     isAdmin,
+        //     modelAircraft,
+        //     email,
+        //     username,
+        //     password,
+        //   }
+        // );
       },
       addAirliner: async (parent, { airlinerID, airlinerName }) => {
-        return Airliner.findOneAndUpdate(
+        return airlinerID.findOneAndUpdate(
           { _id: airlinerID },
           {
             $addToSet: { airlinerName: { airlinerName } },
@@ -44,15 +49,15 @@ export const resolvers = {
         );
       },
       removeAirliner: async (parent, { airlinerID }) => {
-        return Airliner.fineOneAndDelete({ _id: airlinerID });
+        return airlinerID.fineOneAndDelete({ _id: airlinerID });
       },
       removeAirliner: async (parent, { airlinerID, airlinerName }) => {
-        return Airliner.destroy({ _id: airlinerID }, { airlinerName });
+        return airlinerID.destroy({ _id: airlinerID }, { airlinerName });
       },
     },
   }
 };
-console.log(resolvers.Airliner.Query.addAirliner);
+console.log(resolvers.Airliner.Query.airliner);
 
 export const typeDefs = gql`
     type Airliner {
@@ -64,6 +69,18 @@ export const typeDefs = gql`
         email: String
         password: String
   }
+  type Auth {
+    token: ID!
+    airliner: Airliner
+  }
+    type Query {
+      airliner(airlinerID: ID!): Airliner,
+      airliners(airlinerID: ID!): [Airliner]
+    }
+
+    type Mutation {
+      addAirliner(airlinerName: String!, isAdmin: Boolean!, modelAircraft: String, username: String! , email: String! , password: String!): Auth
+    }
 `;
 
 const schema = makeExecutableSchema({
