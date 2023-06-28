f7f0eeec950171e0e4e34cd7f4066ddc1499578b
BinaryBitBytes <117mwc117@gmail.com>
2023-06-27 21:49:19 -0400

    resolved server index.js and server/src/index.js with the typedefs and resolver

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
 // import { ApolloServer, startStandaloneServer, gql } from 'apollo-server';
 import { ApolloServer } from 'apollo-server'
-import { typeDefs, resolver } from '../src/typeDef-Resolvers/index.js'
+import typedefs, { resolver } from '../src/typeDef-Resolvers/index.js'
 // import { resolvers } from '../src/typeDef-Resolvers/index.js'
 
 // const server = new startStandaloneServer({ typeDefs, resolvers });
-const server = new ApolloServer(typeDefs, resolver)
+const server = new ApolloServer(typedefs, resolver)
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
