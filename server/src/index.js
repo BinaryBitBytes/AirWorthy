c6fedc419802cda08b3b0eff70e92959925df08e
BinaryBitBytes <117mwc117@gmail.com>
2023-06-27 22:08:21 -0400

    was able to resolve server/server.js and get the node to run the typedefs and resolvers completely to connect to the server on apollo

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,8 @@
-// import { ApolloServer, startStandaloneServer, gql } from 'apollo-server';
-import { ApolloServer } from 'apollo-server'
-import typedefs, { resolver } from '../src/typeDef-Resolvers/index.js'
-// import { resolvers } from '../src/typeDef-Resolvers/index.js'
+import { ApolloServer } from 'apollo-server';
+import typedefs from '../../server/src/typeDef-Resolvers/index.js';
+import { resolver } from '../../server/src/typeDef-Resolvers/index.js';
 
-// const server = new startStandaloneServer({ typeDefs, resolvers });
-const server = new ApolloServer(typedefs, resolver)
+const server = new ApolloServer({ typeDefs: typedefs, resolvers: resolver });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
