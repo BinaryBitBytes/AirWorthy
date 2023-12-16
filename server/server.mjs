bd7880cb2893b876cd7de5f4f3a853ba10533598
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2023-12-16 14:41:32 -0500

    working on server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -1,12 +1,15 @@
 import * as apollo from "apollo-server-core";
 import * as apolloServerExprress from "apollo-server-express";
-import express from "express";
+import * as express from "express";
 import buildSubgraphSchema from "@apollo/subgraph";
 import * as startStandaloneServer from "@apollo/server/standalone";
 import resolvers from "./src/typeDef-Resolvers/Resolvers/resolvers.mjs";
 import typeDefs from "./src/typeDef-Resolvers/Schema/typeDef.mjs";
 import { connectDB } from "./config/connection.mjs";
 
+const Express = () => {
+  return express;
+};
 const ApolloServerExpress = () => {
   return apolloServerExpress;
 };
@@ -25,12 +28,26 @@ console.log(typeof ApolloServer);
 
 // connecting to the mongo database
 // Global function to start the server asynchronously
-const startServer = async () => {
-  //! const resolvers = await import ('./src/typeDef-Resolvers/Resolvers/resolvers.mjs')
-  // This creates an Express application
-  // // express = await express('express')
-  const app = express();
-  app.use(express);
+const startServer = async () => 
+  {
+  /* //! const resolvers = await import ('./src/typeDef-Resolvers/Resolvers/resolvers.mjs')
+  ☺/ This creates an Express application
+  / // express = await express('express')
+  */
+  const app = Express();
+  // app.use(Express);
+  app.get('/', function(req, res, next) {
+      next()
+    })
+  }
+  console.log(`Listening on 3000 START`)
+  app.listen(3000)
+  console.log(`Listening on 3000 END STARTING PHASE`)
+  /*////////////////////////////////////////////////// */
+  /*////////////////////////////////////////////////// */
+  /*////////////////////////////////////////////////// */
+  /*////////////////////////////////////////////////// */
+
   // This creates the instance of the Apollo server with the typeDefs & resolvers
   const server = async () => {
     // new ApolloServer({
@@ -49,6 +66,7 @@ const startServer = async () => {
     // await new apolloServer.listen({ port: 3069 });
     // await apolloServer.listen({ port: 3069 });
   };
+  /*////////////////////////////////////////////////// */
   console.log(server.apolloServer);
   await server();
   //! testing
@@ -57,7 +75,7 @@ const startServer = async () => {
   //!;
   // await server.listen()
   // This applies the Apollo Server Middleware into the Express application
-  server.applyMiddleware({ app });
+  server.applyMiddleware(app);
   // This starts the server and listens on the respected port address
   app.listen({ port: 3069 }, () => {
     console.log(
@@ -77,6 +95,7 @@ console.info(`Starting function: connectDB`);
 connectDB();
 // // startServer().catch((error) => console.log(error))
 
+//
 /* --------------------------------
   import { buildSubgraphSchema } from '@apollo/subgraph';
   import * as buildSubgraphSchema from '@apollo/subgraph';
@@ -95,3 +114,4 @@ connectDB();
   // import { prototype } from "events";
   // import { sample } from "lodash";
   */
+ //
