afbae939ae9a80c0ce208d8b0d8068d20dfe2cbe
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2024-01-16 16:18:52 -0500

    24.1.16 push of Manager.mjs models

--- server/src/models/Manager.mjs
+++ server/src/models/Manager.mjs
@@ -1,47 +1,60 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 // const { model } = mongoose
-const model = () => {
-  return mongoose;
-};
-const Manager = new mongoose.Schema(
-  {
-    id: {
-      type: Number,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    managerName: { type: String, required: true, unique: true },
-    isAdmin: { type: Boolean, enum: [true] },
-    onProject: [{ type: String }],
-    userName: String,
-    email: String,
-    password: String,
-  },
-  {
-    hooks: {
-      beforeCreate: async function (newManagerData) {
-        newManagerData.password = await bcrypt.hash(
-          newManagerData.password,
-          10
-        );
-        return newManagerData;
-      },
-      beforeUpdate: async function (updatedManagerData) {
-        updatedManagerData.password = await bcrypt.hash(
-          updatedManagerData.password,
-          10
-        );
-        return updatedManagerData;
-      },
-    },
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "Manager",
-  }
-);
+// const model = () => {
+//   return mongoose;
+// };
 
-const ManagerModel = model("Manager", Manager);
+class ManagerModel {
+  constructor() {
+    this.schema = new mongoose.Schema(
+      {
+        id: {
+          type: Number,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        managerName: { type: String, required: true, unique: true },
+        isAdmin: { type: Boolean, enum: [true] },
+        onProject: [{ type: String }],
+        userName: String,
+        email: String,
+        password: String,
+      },
+      {
+        hooks: {
+          beforeCreate: async function (newManagerData) {
+            newManagerData.password = await bcrypt.hash(
+              newManagerData.password,
+              10
+            );
+            return newManagerData;
+          },
+          beforeUpdate: async function (updatedManagerData) {
+            updatedManagerData.password = await bcrypt.hash(
+              updatedManagerData.password,
+              10
+            );
+            return updatedManagerData;
+          },
+        },
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "Manager",
+      }
+    );
+    this.model = mongoose.model("Manager", this.schema);
+  }
+}
+
+console.log(`
+  Console Log(typeof) ===  ${typeof ManagerModel} 
+-----------------
+  Console.log(ManagerModel)
+  ${ManagerModel}
+------------
+  `);
+
 export default ManagerModel;
