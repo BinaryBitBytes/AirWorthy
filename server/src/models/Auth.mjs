18ebe4a02af0aa6d556c4df4fcdefe4ca976a931
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2024-01-16 15:59:18 -0500

    updated Auth.mjs in server/src/models/

--- server/src/models/Auth.mjs
+++ server/src/models/Auth.mjs
@@ -2,52 +2,75 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const model = () => {
-  return mongoose;
-};
+// const Model = () => {
+//   return mongoose;
+// };
 // import { AuthTypeDefs } from "../typeDef-Resolvers/authSchema.js";
-const Auth = new mongoose.Schema(
-  {
-    id: {
-      type: Number,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    token: {
-      type: String,
-      allowNull: false,
-      primaryKey: false,
-    },
-    userID: [{ type: String }],
-    user: String,
-    userName: String,
-    email: String,
-    password: String,
-    isAdmin: { type: Boolean, enum: [true] },
-  },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
+class AuthModel {
+  constructor() {
+    this.schema = new mongoose.Schema(
+      {
+        id: {
+          type: Number,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        token: {
+          type: String,
+          allowNull: false,
+          primaryKey: false,
+        },
+        userID: [{ type: String }],
+        user: String,
+        userName: String,
+        email: String,
+        password: String,
+        isAdmin: { type: Boolean, enum: [true] },
       },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
-      },
-    },
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "Auth",
-  }
-);
-const AuthModel = model("Auth", Auth);
+      {
+        hooks: {
+          beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+          beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(
+              updatedUserData.password,
+              10
+            );
+            return updatedUserData;
+          }, //? closing: beforeUpdate hook
+        }, //? closing: hooks
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "Auth",
+      }
+    );
+    //? End of Schema within the class constructor
+    //  (((((((((Start Model()))))))))))
+    //? Now need to create model within the class
+    this.model = mongoose.model("Auth", this.schema, this.options);
+    // ((((((((((End Model()))))))))))
+  } //! End of constructor
+} //! End of class
+
+//* @Params: METHODS > \\can be defined below: Example: //
+//   findUserById(userId) {
+//     return this.model.findById(userId);
+//   }
+// }
+
+console.log(`Type of AuthModel = ` + typeof AuthModel);
+console.log(`----------------------------------------------------`);
+
+console.log(`AuthModel @ console.log(AuthModel) = 
+${AuthModel}`);
+
+console.log(`-------------------------Before Export-------------------------`);
 export default AuthModel;
+console.log(`------------------------After Export---------------------------`);
 
 //! @BinaryBitBytes 1.16.24 - original version @ branch: Server.12.15.23
 // const Auth = new mongoose.Schema(
