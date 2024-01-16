eca3c930293434d6cec71b8d22a5bd6c71002de7
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2024-01-16 16:13:43 -0500

    24.1.16 push of Project.mjs models

--- server/src/models/Project.mjs
+++ server/src/models/Project.mjs
@@ -1,27 +1,42 @@
 import mongoose from "mongoose";
-const model = () => {
-  return mongoose;
-}; // import { resolvers } from '../typeDef-Resolvers/projectSchema.js'
+//? import bcrypt from "bcrypt";
 
-const Project = new mongoose.Schema(
-  {
-    id: {
-      type: Number,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    projectName: [{ type: String }],
-    workDescription: [{ body: String }],
-    startingDate: [{ date: Date }],
-    modelAircraft: [{ type: Number }],
-  },
-  {
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "Project",
-  }
-);
-const ProjectModel = model("Project", Project);
+// const model = () => {
+//   return mongoose;
+// }; // import { resolvers } from '../typeDef-Resolvers/projectSchema.js'
+
+class ProjectModel {
+  constructor() {
+    this.schema = new mongoose.Schema(
+      {
+        id: {
+          type: Number,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        projectName: [{ type: String }],
+        workDescription: [{ body: String }],
+        startingDate: [{ date: Date }],
+        modelAircraft: [{ type: Number }],
+      },
+      {
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "Project",
+      }
+    ); //? End of Schema
+    //? Start of Model
+    this.model = mongoose.model("Project", this.schema);
+    //? End of Model
+  } //? End of Constructor
+} //> End of Class
+console.log(`
+  Console Log(typeof) ===  ${typeof ProjectModel} 
+-----------------
+  Console.log(ProjectModel)
+  ${ProjectModel}
+------------
+  `);
 export default ProjectModel;
